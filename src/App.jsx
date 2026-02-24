764e7a392b7f0fe87d22274d541ba45426590283
Esra Fernanda Siregar <esrafernanda2@gmail.com>
2026-02-25 07:58:08 +0800

    Installed vercell Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import Tabs from "./components/Tabs";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
